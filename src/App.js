74d66d53244ef1d93977ae053e6498fe9d827f7a
ElderSinOfFury <126672739+ElderSinOfFury@users.noreply.github.com>
2023-03-05 17:00:31 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import s from "./App.module.scss";
 import Header from "./Components/Widgets/Header/Header";
-import Body from "./Components/Widgets/Body/Body";
 
 function App() {
   return (
-    <div className={s.app}>
-      <Header />
-      <div className={s.body_container}>
-        <Body />
-      </div>
+    <div className={s.App}>
+    <Header />
     </div>
   );
 }
--- src/Components/Widgets/Navbar/Navbar.jsx
+++ src/Components/Widgets/Navbar/Navbar.jsx
@@ -1,16 +1,14 @@
-import s from "./Navbar.module.scss";
+import s from "./Navbar.module.scss"
+import Icon from "./../../../assets/images/IconNavVector.svg"
 
 const Navbar = () => {
-  return (
-    <div className={s.navbar}>
-      <a href="/" className={s.link}>
-        Movie
-      </a>
-      <a href="/" className={s.link}>
-        TV Show
-      </a>
-    </div>
-  );
-};
+    return ( 
+    <div> 
+    <a href="/">Movies</a>
+    <a href="/">TV Shows</a>
+    <a href="/">Suggest me</a>    
+    <a href="/"><img src={icon} alt="logo" /></a>    
+    </div> );
+}
 
 export default Navbar;
